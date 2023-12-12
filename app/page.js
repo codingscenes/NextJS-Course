9c9369b306c170e9739081e2c7b6517500b89030
Coding Scenes <codingscenes@gmail.com>
2023-12-12 17:30:41 +0530

    inital

--- app/page.js
+++ app/page.js
@@ -1,48 +1,95 @@
-import ImageSlider from '@/components/image-slider/image-slider';
-import Link from 'next/link';
-import styles from './page.module.css';
+import Image from 'next/image'
+import styles from './page.module.css'
 
 export default function Home() {
   return (
-    <div className='container mt-5'>
-      <div className='row'>
-        <div className='col-6'>
-          <h2 className={styles.pageTitle}>A Place for Travellers!</h2>
-          <p>
-            Welcome to our Travel Guide App! Explore and discover amazing destinations across India, shared by our
-            community of avid travelers. Get firsthand tips, experiences, and advice from people who've been there.
-            Whether you're planning your next adventure or just want to travel vicariously from the comfort of your
-            home, our Travel Guide App is the perfect companion. Start your journey with us today.
-          </p>
-          <Link href='/destinations' className='btn btn-dark'>
-            Explore Destinations
-          </Link>
-        </div>
-        <div className='col-6'>
-          <ImageSlider />
-        </div>
-      </div>
-      <div className='row'>
-        <div className='col'>
-          <h3 className={styles.pageTitle}>Share your travelling tips!</h3>
-          <p>
-            Sharing your travel experiences with others is a great way to help them plan their next trip. You can share
-            your tips and advice on our Travel Guide App by creating a post about your trip. You can also share your
-            photos and videos from the trip, so others can see what it was like. We hope you enjoy using our Travel
-            Guide App!
-          </p>
+    <main className={styles.main}>
+      <div className={styles.description}>
+        <p>
+          Get started by editing&nbsp;
+          <code className={styles.code}>app/page.js</code>
+        </p>
+        <div>
+          <a
+            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            By{' '}
+            <Image
+              src="/vercel.svg"
+              alt="Vercel Logo"
+              className={styles.vercelLogo}
+              width={100}
+              height={24}
+              priority
+            />
+          </a>
         </div>
       </div>
-      <div className='row'>
-        <div className='col'>
-          <h3 className={styles.pageTitle}>Get destination tips!</h3>
-          <p>
-            Our Travel Guide App is a great way to find new destinations and plan your next trip. You can search for
-            destinations by country, city, or region. You can also filter destinations by type of activity (e.g.,
-            hiking, beach, skiing). We hope you enjoy using our Travel Guide App!
-          </p>
-        </div>
+
+      <div className={styles.center}>
+        <Image
+          className={styles.logo}
+          src="/next.svg"
+          alt="Next.js Logo"
+          width={180}
+          height={37}
+          priority
+        />
       </div>
-    </div>
-  );
+
+      <div className={styles.grid}>
+        <a
+          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+          className={styles.card}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <h2>
+            Docs <span>-&gt;</span>
+          </h2>
+          <p>Find in-depth information about Next.js features and API.</p>
+        </a>
+
+        <a
+          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+          className={styles.card}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <h2>
+            Learn <span>-&gt;</span>
+          </h2>
+          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
+        </a>
+
+        <a
+          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+          className={styles.card}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <h2>
+            Templates <span>-&gt;</span>
+          </h2>
+          <p>Explore starter templates for Next.js.</p>
+        </a>
+
+        <a
+          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+          className={styles.card}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <h2>
+            Deploy <span>-&gt;</span>
+          </h2>
+          <p>
+            Instantly deploy your Next.js site to a shareable URL with Vercel.
+          </p>
+        </a>
+      </div>
+    </main>
+  )
 }
