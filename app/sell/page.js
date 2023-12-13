125ff8edc501da064be5bd7ce37a39a5010cb6be
Coding Scenes <codingscenes@gmail.com>
2023-12-13 14:44:20 +0530

    added server function

--- app/sell/page.js
+++ app/sell/page.js
@@ -3,12 +3,24 @@ import classNames from 'classnames';
 import styles from './page.module.css';
 
 export default function SellPage() {
+  async function publishProduct(formData) {
+    'use server'; //This will create server action and is gauranted that this function will only execute on the server. so here we mark it that it will execute on server not on client and it is a server action.
+    // FormData() // js class to handle submitted data
+    const product = {
+      name: formData.get('name'),
+      description: formData.get('description'),
+      price: formData.get('price'),
+      imageUrl: formData.get('image'), //should be stored on filesystem and only url should be stored in imageUrl db
+      sellerId: 1, // should be logged in user id
+    };
+    console.log(product);
+  }
   return (
     <div className='container mb-4'>
       <div className='row mx-auto justify-content-center'>
         <div className='col-md-8'>
           <h1 className='fancy mt-4'>Sell your product</h1>
-          <form className={styles.form}>
+          <form className={styles.form} action={publishProduct}>
             <div className='form-group mt-4'>
               <label htmlFor='name' className={styles.label}>
                 PRODUCT NAME
@@ -18,6 +30,8 @@ export default function SellPage() {
                 className='form-control bg-dark text-white'
                 id='name'
                 placeholder='Enter product name'
+                name='name'
+                required
               />
             </div>
             <div className='form-group mt-4'>
@@ -29,6 +43,8 @@ export default function SellPage() {
                 id='description'
                 rows='4'
                 placeholder='Enter product description'
+                name='description'
+                required
               ></textarea>
             </div>
             <div className='form-group mt-4'>
@@ -40,6 +56,8 @@ export default function SellPage() {
                 className={classNames('form-control bg-dark text-white', styles.price)}
                 id='price'
                 placeholder='Enter product price'
+                name='price'
+                required
               />
             </div>
             <ImagePicker label='IMAGE' name='image' />
