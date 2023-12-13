3fb2f27fefced37fb390091458f8621a840fce75
Coding Scenes <codingscenes@gmail.com>
2023-12-13 15:42:51 +0530

    storing uploaded images completed

--- libs/actions.js
+++ libs/actions.js
@@ -1,12 +1,18 @@
-'use server'; // important to have so all the function defined here will be treated as server functions
+'use server';
+import { redirect } from 'next/navigation';
+import { saveProduct } from './products';
+
+// important to have so all the function defined here will be treated as server functions
 
 export async function publishProduct(formData) {
   const product = {
     name: formData.get('name'),
     description: formData.get('description'),
     price: formData.get('price'),
-    imageUrl: formData.get('image'), //should be stored on filesystem and only url should be stored in imageUrl db
+    image: formData.get('image'), //should be stored on filesystem and only url should be stored in imageUrl db
     sellerId: 1, // should be logged in user id
   };
   console.log(product);
+  await saveProduct(product);
+  redirect('/products');
 }
--- libs/products.js
+++ libs/products.js
@@ -1,4 +1,7 @@
 const { PrismaClient } = require('@prisma/client');
+const slugify = require('slugify');
+const xss = require('xss');
+const fs = require('node:fs');
 
 const prisma = new PrismaClient();
 
@@ -46,4 +49,33 @@ export async function getProductBySlug(slug) {
   } finally {
     await prisma.$disconnect();
   }
+}
+
+export async function saveProduct(product) {
+  //npm install slugify xss (to santize the user content)
+  const slug = slugify(product.name, { lower: true });
+  const description = xss(product.description);
+  // image should be stored on file system
+  const extension = product.image.name.split('.').pop();
+  const fileName = `${slug}.${extension}`; //avoid accidentally override other images
+  // it will create a stream that allows to write data to certain file
+  // it also needs path to which we wanna write and then it return a string object which we can then use to write to that path
+  const stream = fs.createWriteStream(`public/images/${fileName}`);
+  const bufferedImage = await product.image.arrayBuffer();
+  //           Thing we wana right, second is function which executes when we done writing
+  stream.write(Buffer.from(bufferedImage), (error) => {
+    if (error) {
+      throw new Error('Saving image failed!');
+    }
+  });
+  const imageUrl = `/images/${fileName}`;
+  await prisma.product.create({
+    data: {
+      name: product.name,
+      description: description,
+      sellerId: +product.sellerId,
+      price: +product.price,
+      imageUrl: imageUrl,
+    },
+  });
 }
