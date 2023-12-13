8f3b63bca9c57c9ae9acf65ab145a9292b25c9c9
Coding Scenes <codingscenes@gmail.com>
2023-12-13 16:07:04 +0530

    worked on server side input validation for the form

---
+++ app/sell/error.js
@@ -0,0 +1,15 @@
+'use client'; //also catch error for client including server side rendering
+export default function Error({ error }) {
+  //actual error message is passed from the getServerSideProps function
+  // and it is not exposed to the user by nextjs to avoid security issues
+  return (
+    <div className='container mt-4'>
+      <div className='row'>
+        <div className='col-12'>
+          <h1 className='fancy'>An error occurred!</h1>
+          <p>Failed to Create products. Please check the inputs!</p>
+        </div>
+      </div>
+    </div>
+  );
+}
--- libs/actions.js
+++ libs/actions.js
@@ -4,6 +4,9 @@ import { saveProduct } from './products';
 
 // important to have so all the function defined here will be treated as server functions
 
+function isInvalidText(text) {
+  return !text || text.trim() === '';
+}
 export async function publishProduct(formData) {
   const product = {
     name: formData.get('name'),
@@ -13,6 +16,21 @@ export async function publishProduct(formData) {
     sellerId: 1, // should be logged in user id
   };
   console.log(product);
+
+  // server side input validation; (later)
+  // if (!product.name || product.name.trim() === '') {
+
+  // }
+  if (
+    isInvalidText(product.name) ||
+    isInvalidText(product.description) ||
+    isInvalidText(product.price) ||
+    !product.image ||
+    product.image.size === 0
+  ) {
+    throw new Error('Invalid input received!');
+  }
+
   await saveProduct(product);
   redirect('/products');
 }
